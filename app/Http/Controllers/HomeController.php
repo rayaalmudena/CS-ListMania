97ba8a8277d1a380499df8cb0d9768e950f13497
rayaalmudena <raya.almudena@gmail.com>
2020-05-18 22:43:12 +0200

    da

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,11 +23,11 @@ class HomeController extends Controller
      */
     public function index()
     {   
-        $movies = DB::select('SELECT DISTINCT saved_objects.api_id, saved_objects.name_object, round(AVG(saved_objects.rating),2) as rating, caches.image FROM saved_objects LEFT JOIN caches ON saved_objects.api_id = caches.api_id WHERE saved_objects.type="movies" and rating is not null  GROUP BY saved_objects.api_id, saved_objects.name_object, caches.image ORDER BY saved_objects.rating DESC LIMIT 10');
+        $movies = DB::select('SELECT DISTINCT saved_objects.api_id, saved_objects.name_object, round(AVG(saved_objects.rating),2) as rating, caches.image FROM saved_objects LEFT JOIN caches ON saved_objects.api_id = caches.api_id WHERE saved_objects.type="movies" and rating is not null  GROUP BY saved_objects.api_id, saved_objects.name_object, caches.image ORDER BY saved_objects.rating, saved_objects.name_object, saved_objects.api_id DESC LIMIT 10');
 
-        $shows = DB::select('SELECT DISTINCT saved_objects.api_id, saved_objects.name_object, round(AVG(saved_objects.rating),2) as rating, caches.image FROM saved_objects LEFT JOIN caches ON saved_objects.api_id = caches.api_id WHERE saved_objects.type="series" and rating is not null  GROUP BY saved_objects.api_id, saved_objects.name_object, caches.image ORDER BY saved_objects.rating DESC LIMIT 10');
+        $shows = DB::select('SELECT DISTINCT saved_objects.api_id, saved_objects.name_object, round(AVG(saved_objects.rating),2) as rating, caches.image FROM saved_objects LEFT JOIN caches ON saved_objects.api_id = caches.api_id WHERE saved_objects.type="series" and rating is not null  GROUP BY saved_objects.api_id, saved_objects.name_object, caches.image ORDER BY saved_objects.rating, saved_objects.name_object, saved_objects.api_id DESC LIMIT 10');
 
-        $books = DB::select('SELECT DISTINCT saved_objects.api_id, saved_objects.name_object, round(AVG(saved_objects.rating),2) as rating, caches.image FROM saved_objects LEFT JOIN caches ON saved_objects.api_id = caches.api_id WHERE saved_objects.type="books" and rating is not null  GROUP BY saved_objects.api_id, saved_objects.name_object, caches.image ORDER BY saved_objects.rating DESC LIMIT 10');
+        $books = DB::select('SELECT DISTINCT saved_objects.api_id, saved_objects.name_object, round(AVG(saved_objects.rating),2) as rating, caches.image FROM saved_objects LEFT JOIN caches ON saved_objects.api_id = caches.api_id WHERE saved_objects.type="books" and rating is not null  GROUP BY saved_objects.api_id, saved_objects.name_object, caches.image ORDER BY saved_objects.rating, saved_objects.name_object, saved_objects.api_id DESC LIMIT 10');
        // dd($series);
 
         return view('home', ['movies'=>$movies, 'shows'=>$shows, 'books'=>$books]);
