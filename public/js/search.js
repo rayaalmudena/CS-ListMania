d78001add2b4f076e2ed00cc579e0f6bbb0ad5cb
rayaalmudena <raya.almudena@gmail.com>
2020-05-20 01:55:03 +0200

    ads

--- public/js/search.js
+++ public/js/search.js
@@ -4,6 +4,13 @@ $( document ).ready(function() {
 		searchMovieOrShowByNameAPI(searched);
 		searchBookByNameAPI(searched);
 	}
+
+	$( ".figure" ).parent().parent().each(function() {      
+		id=$(this).attr("type");
+		type=$(this).attr("id");
+     	$(html).not('#'+id+'[type="'+type+'"]:first').remove();
+     
+  	});
 	
 	
 });
