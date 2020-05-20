75cff1d8a89533e5f64cba6ab506a1c191833961
rayaalmudena <raya.almudena@gmail.com>
2020-05-20 02:19:29 +0200

    ads

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -14,7 +14,19 @@ class SearchController extends Controller
      public function search(Request $request)
     { 
 	   	$search = $request->searchThis;
-	    $user = DB::select('SELECT DISTINCT api_id, type, image, title FROM caches WHERE LOWER(title) like LOWER("%'.$search.'%")  ORDER BY title DESC ');
+	    $user = DB::select('SELECT DISTINCT api_id, type, title FROM caches WHERE LOWER(title) like LOWER("%'.$search.'%") ORDER BY title DESC ');
+
+	    $imageAdd = DB::select('SELECT DISTINCT api_id, type, image  FROM caches WHERE LOWER(title) like LOWER("%'.$search.'%") group by api_id, type, image, ORDER BY title DESC ');
+
+	    foreach ($user as $user1) {
+               foreach ($imageAdd as $image) {
+                   if($image->api_id==$user1->api_id and $image->type==$user1->type){
+                    $user1->image=$image->image;
+                   }
+               }
+            }
+
+
 	    if (count ( $user ) > 0)
 	        return view ( 'search' )->withDetails ( $user )->withQuery ( $search );
 	    else
