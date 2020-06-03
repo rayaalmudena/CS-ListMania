08aa9a48c12eb8faef80013913af00097c7e0da7
rayaalmudena <raya.almudena@gmail.com>
2020-06-03 19:19:22 +0200

    errors real

--- public/js/detail/movieShow/main.js
+++ public/js/detail/movieShow/main.js
@@ -2,11 +2,13 @@ $( document ).ready(function() {
     
     if (window.location.href.indexOf("movie") != -1){ 
 
-      searchMovieOrShowByIdDBType($(".objectSearch").attr("id"), "movies");  
-   
+	//searchMovieOrShowByIdDBType($(".objectSearch").attr("id"), "movies");  
+	searchMovieOrShowByIdDB($(".objectSearch").attr("id"));  
+
     }else{
 
-     searchMovieOrShowByIdDBType($(".objectSearch").attr("id"), "series");  
+    // searchMovieOrShowByIdDBType($(".objectSearch").attr("id"), "series");  
+     searchMovieOrShowByIdDB($(".objectSearch").attr("id"));  
     
     }
   	
